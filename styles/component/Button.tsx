531f530ed92183d93aa40267b0ca8dea72c470dd
senabiosenpai <84292860+senabiosenpai@users.noreply.github.com>
2021-11-27 09:28:48 -0300

    Update Button.tsx

--- styles/component/Button.tsx
+++ styles/component/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonContainer } from "../home";
 export default function Button({ label }: { label: string }) {
   return (
     <ButtonContainer
-      onClick={() => window.open("https://discord.gg/PcX2w8GqaW", "_blank")}
+      onClick={() => window.open("https://discord.gg/5gT6EAGCm3", "_blank")}
     >
       <div className="button">{label}</div> <div className="effect"></div>
     </ButtonContainer>
